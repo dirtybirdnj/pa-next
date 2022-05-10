7158a6599ba793df330d8e0fe89deed273cd569b
Mat Gilbert <matgilbert@gmail.com>
2022-05-09 22:53:12 -0400

    Added placekitten products, light theme

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Image from 'next/image';
 
 export const Products = (): JSX.Element => {
   return (
@@ -14,6 +15,31 @@ export const Products = (): JSX.Element => {
         Our combination of bespoke design with unconventional rendering options
         creates experiences that will delight your customers and leave a
         memorable impression on guests, family and friends.
+      </p>
+
+      <h2>Business Cards</h2>
+      <p>
+        <Image
+          src="http://placekitten.com/200/300"
+          width={200}
+          height={300}
+          alt="Business Cards"
+        />
+        Provide your contact information with a flair. Leave a lasting
+        impression that will impress clients and enhance your networking
+        experience.
+      </p>
+
+      <h2>Menus</h2>
+      <p>
+        For Restaurants, Weddings, and any service industry where a physical
+        copy of written options is essential. We combine
+      </p>
+
+      <h2>Documents</h2>
+      <p>
+        We can create custom artwork to commemorate your significant milestone
+        or{' '}
       </p>
     </Layout>
   );
